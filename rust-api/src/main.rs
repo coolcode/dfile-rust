3a6df09b23fad3352cd4e8fdb6d4f34e71910863
Bruce Lee <coolcode@live.com>
2019-10-18 23:08:03 +0800

    remove useless code

--- rust-api/src/main.rs
+++ rust-api/src/main.rs
@@ -92,10 +92,9 @@ fn hello() -> &'static str {
 }
 
 #[post("/", data = "<data>")]
-// signature requires the request to have a `Content-Type` //, conf: State<'_, Config>
+// signature requires the request to have a `Content-Type`
 fn upload(content_type: &ContentType, data: Data) -> &'static str {
-    // this and the next check can be implemented as a request guard but it seems like just
-    // more boilerplate than necessary
+    // this and the next check can be implemented as a request guard but it seems like just more boilerplate than necessary
     if !content_type.is_form_data() {
 //        return Err(Custom(
 //            Status::BadRequest,
@@ -112,77 +111,39 @@ fn upload(content_type: &ContentType, data: Data) -> &'static str {
 || "`Content-Type: multipart/form-data` boundary param not provided"
     ).expect("Error in boundary");
 
-    //let mut files = Vec::new();
     let mut multipart = Multipart::with_body(data.open(), boundary);
-    //let mut file_data: FileData;
     let mut r = "";
     while let Some(entry) = multipart.read_entry().expect("Error in read entry") {
         let field_name = entry.headers.name.to_string();
         println!("field_name: {}", &field_name);
+        let filename = entry.headers.filename.unwrap();
+        println!("filename: {}", &filename);
         let content_type = entry.headers.content_type.unwrap().to_string();
-        //  .map(|mime| Mime::from_str(&mime.to_string()).unwrap());
         println!("content_type: {}", &content_type);
-        let mut data = entry.data;
         let mut buffer: Vec<u8> = Vec::new();
-        //data.stream_to(&mut buffer);
-        //let mut buffer =  data.fill_buf().expect("Error in fil_buf").to_vec();
-        //let mut buffer = io::BufRead::fill_buf(&data).expect("Error in fil_buf").to_vec();
-        let mut sum_c = 0u64;
         let mut temp = [0u8; 4096];
-
-        while let c = data.read(&mut temp).expect("Error in read data") {
+        let mut entry_data = entry.data;
+        while let c = entry_data.read(&mut temp).expect("Error in read data") {
             if c == 0 {
                 break;
             }
-
-            sum_c += c as u64;
-
             buffer.extend_from_slice(&temp[..c]);
         }
 
         println!("buffer: {}", buffer.len());
-//        let mut file_data = FileData { name: field_name, content_type: content_type, buffer: buffer };
-//        files.push(file_data);
-//
-//        let mut reader = file.buffer;
-        r = upload_file_to_s3(&buffer);
+        r = upload_file_to_s3(&buffer, &filename, &content_type);
+        //TODO: multiple files
         break;
     }
 
     return r;
-
-//    match process_upload(boundary, data) {
-//        Ok(&file) => {
-//            //Ok(Stream::from(Cursor::new(resp)))
-////            for file in files {
-////                println!("file: {}", file.name);
-////            }
-////            let r;
-////            if files.len() == 0 {
-////                r = "No file!".to_owned();
-////            } else {
-////                let file = files.first().unwrap().buffer;
-////
-////
-////                let mut reader = file.as_mut_slice();
-////                //io::copy(&mut reader, &mut writer)?;
-////
-////                r = upload_file_to_s3(&reader).to_owned();
-////            }
-//
-//            let mut reader = file.buffer;
-//            let r = upload_file_to_s3(&reader).to_owned();
-//            Ok(r)
-//        }
-//        Err(err) => Err(Custom(Status::InternalServerError, err.to_string()))
-//    }
 }
 
 fn get_env_value(name: &str) -> String {
     env::var(name).expect(format!("No {:?} environment variable found", name).as_str())
 }
 
-fn upload_file_to_s3(contents: &[u8]) -> &'static str {
+fn upload_file_to_s3(contents: &[u8], meta_filename: &str, content_type: &str) -> &'static str {
     let S3_REGION = get_env_value("S3_REGION");
     let S3_ENDPOINT = get_env_value("S3_ENDPOINT");
     let S3_KEY = get_env_value("S3_KEY");
@@ -206,106 +167,10 @@ fn upload_file_to_s3(contents: &[u8]) -> &'static str {
         region,
     );
 
-    upload_file(&s3client, "test", &filename, &contents);
+    upload_file(&s3client, "test", &filename, &contents, &meta_filename, &content_type);
     println!("[Upload] Took {:?}", Instant::now().duration_since(start));
 
     Box::leak(filename.into_boxed_str())
-}
-
-//fn process_upload(boundary: &str, data: Data) -> io::Result<FileData> {
-//    let mut files = Vec::new();
-//    let mut multipart = Multipart::with_body(data.open(), boundary);
-//    let mut file_data: FileData;
-//    while let Some(entry) = multipart.read_entry()? {
-//        let field_name = entry.headers.name.to_string();
-//        println!("field_name: {}", &field_name);
-//        let content_type = entry.headers.content_type.unwrap().to_string();
-//        //  .map(|mime| Mime::from_str(&mime.to_string()).unwrap());
-//        println!("content_type: {}", &content_type);
-//        let mut data = entry.data;
-//        let mut buffer: Vec<u8> = Vec::new();
-//        //data.stream_to(&mut buffer);
-//        //let mut buffer =  data.fill_buf().expect("Error in fil_buf").to_vec();
-//        //let mut buffer = io::BufRead::fill_buf(&data).expect("Error in fil_buf").to_vec();
-//        let mut sum_c = 0u64;
-//        let mut temp = [0u8; 4096];
-//
-//        while let c = data.read(&mut temp)? {
-//            if c == 0 {
-//                break;
-//            }
-//
-//            sum_c += c as u64;
-//
-//            buffer.extend_from_slice(&temp[..c]);
-//        }
-//
-//        println!("buffer: {}", buffer.len());
-//        file_data = FileData { name: field_name, content_type: content_type, buffer: buffer };
-//        files.push(file_data);
-//
-//        //break;
-//
-////        if let Ok(vi) = options.allowed_fields.binary_search_by(|f| f.field_name.cmp(&field_name))
-////        {
-////            println!("vi: {}", vi);
-////
-////            //let mut buffer = [0u8; 4096];
-////            //let field = options.allowed_fields.remove(vi);
-////            match field.typ {
-////                MultipartFormDataType::File => {
-////                    let mut data = entry.data;
-////                    let mut buffer: Vec<u8> = Vec::new();
-////                    data.stream_to(&mut buffer);
-////                    let file_data = FileData { name: field_name, content_type: content_type, buffer: buffer };
-////                    files.push(file_data);
-////                }
-////            }
-////        };
-//    }
-//
-//    Ok(file_data)
-//    //let mut out = Vec::new();
-//    // let mut buffer: Vec<u8> = Vec::new();
-////    // read the whole file
-//////    let mut contents: Vec<u8> = Vec::new();
-//////    //f.seek(std::io::SeekFrom::Start(0));
-//////    match f.read_to_end(&mut contents) {
-//////
-//////    }
-////    let f = data.open();
-////    f.read_to_end(&mut buffer)?;
-//    //  data.stream_to(&mut buffer);
-//    //Ok(files)
-//    // saves all fields, any field longer than 10kB goes to a temporary directory
-//    // Entries could implement FromData though that would give zero control over
-//    // how the files are saved; Multipart would be a good impl candidate though
-////    match Multipart::with_body(data.open(), boundary).save().temp() {
-////        Full(entries) => process_entries(entries, &mut out)?,
-////        Partial(partial, reason) => {
-////            writeln!(out, "Request partially processed: {:?}", reason)?;
-////            if let Some(field) = partial.partial {
-////                writeln!(out, "Stopped on field: {:?}", field.source.headers)?;
-////            }
-////
-////            process_entries(partial.entries, &mut out)?
-////        }
-////        Error(e) => return Err(e),
-////    }
-////
-////    Ok(out)
-//}
-
-// having a streaming output would be nice; there's one for returning a `Read` impl
-// but not one that you can `write()` to
-fn process_entries(entries: Entries, mut out: &mut Vec<u8>) -> io::Result<()> {
-    {
-        let stdout = io::stdout();
-        let tee = StdoutTee::new(&mut out, &stdout);
-        entries.write_debug(tee)?;
-    }
-
-    writeln!(out, "Entries processed")
 }
 
 fn upload_file(
@@ -313,15 +178,17 @@ fn upload_file(
     bucket: &str,
     dest_filename: &str,
     contents: &[u8],
+    meta_filename: &str,
+    content_type: &str,
 ) {
     let mut metadata = HashMap::new();
-    metadata.insert("name".to_owned(), dest_filename.to_owned());
+    metadata.insert("name".to_owned(), meta_filename.to_owned());
 
     let req = PutObjectRequest {
         bucket: bucket.to_owned(),
         key: dest_filename.to_owned(),
         body: Some(contents.to_owned().into()),
-        content_type: Some("image/png".to_owned()),
+        content_type: Some(content_type.to_owned()),
         acl: Some("public-read".to_owned()),
         metadata: Some(metadata),
         ..Default::default()
